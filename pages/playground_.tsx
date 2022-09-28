5c025e9f0bc2eb44bd5d6468d68179190c572152
LwaziNF <lwazinf@gmail.com>
2022-09-28 19:41:47 +0200

    progress :: making a bit of progress

--- pages/playground_.tsx
+++ pages/playground_.tsx
@@ -1,15 +1,32 @@
-import data from './../src/decent.land.json'
+import data from "./../src/decent.land.json";
+import { select, Selection } from "d3-selection";
+import { useEffect, useRef, useState } from "react";
 
-interface Playground_Props {
-    
-}
- 
+interface Playground_Props {}
+
 const Playground_ = () => {
-    return ( 
-        <div className={`w-full min-h-screen flex flex-col justify-center items-center`}>
-            {data.res.ERC_NFTS.toString()}
-        </div>
-     );
+  const ref = useRef(null)
+  const [selection_, setSelection_] = useState<null | Selection<null, unknown, null, undefined>>(null)
+
+  useEffect(() => {
+    if(!selection_){
+      setSelection_(select(ref.current));
+    } else {
+      selection_
+      .append('rect')
+      .attr('width', 100)
+      .attr('height', 200)
+      .attr('fill', 'red')
+    }
+  }, [selection_])
+
+  return (
+    <div className={`w-full min-h-screen flex flex-col justify-center items-center`}>
+      <svg
+      ref={ref}
+    />
+    </div>
+  )
 }
- 
+
 export default Playground_;
