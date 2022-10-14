e2a016a4eab1a9687b410771d5a5dcb7ac70e44d
LwaziNF <lwazinf@gmail.com>
2022-10-14 20:16:56 +0200

    progress :: need to develop one more element

--- components_new/user/Visuals.tsx
+++ components_new/user/Visuals.tsx
@@ -10,8 +10,9 @@ import { select } from "d3-selection";
 import { createRef, useEffect, useRef, useState } from "react";
 import { Bar, Pie } from "react-roughviz";
 import { useRecoilState } from "recoil";
-import { isDarkMode } from "../../atoms";
+import { extendDash, isDarkMode } from "../../atoms";
 import * as d3 from "d3";
+import { drag } from "d3";
 
 interface VisualsProps {
   data: any;
@@ -19,8 +20,12 @@ interface VisualsProps {
 
 const Visuals = ({ data }: VisualsProps) => {
   const [currentData, setCurrentData] = useState("tag");
+
   // Everything on this element is Light/Dark theme ready..
   const [isDark_, setIsDark_] = useRecoilState(isDarkMode);
+
+  // Clicking "assets/Transactions" reveals one of two dashboard views.. Dash is the Recoil atom which changes the ui
+  const [dash_, setDash_] = useRecoilState(extendDash);
 
   const getData = (userInput: any) => {
     let platform_ = [];
@@ -44,64 +49,318 @@ const Visuals = ({ data }: VisualsProps) => {
 
   const svgRef = createRef<HTMLDivElement>();
 
-  let w = 700;
-  let h = 400;
+  let w = 452;
+  let h = 412;
 
   let z = data.map((obj) => {
-    return !isNaN(parseFloat(obj.fee)) ? parseFloat(obj.fee)*25 : 0
-  })
+    return !isNaN(parseFloat(obj.fee)) ? parseFloat(obj.fee) * 25 : 0;
+  });
 
   useEffect(() => {
-    const simulation = d3.forceSimulation()
-    .force('x', d3.forceX().strength(0.005))
-    .force('y', d3.forceX().strength(0.005))
-    .force('collide', d3.forceCollide((d) => {
-      return radiusScale(parseFloat(d.fee)*10000 + 1.5)
-    }))
-    
+    const simulation = d3
+      .forceSimulation()
+      .force("x", d3.forceX().strength(0.0005))
+      .force("y", d3.forceX().strength(0.0005))
+      .force("charge", d3.forceManyBody().strength(1))
+      .force("centerStage", d3.forceCenter(w / 2, h / 2))
+      .force(
+        "collide",
+        d3.forceCollide((d) => {
+          return radiusScale(parseFloat(d.fee) * 8000) + 1.5;
+        })
+      );
+
     const refAccess = select(svgRef.current)
       .append("svg")
       .attr("width", w)
       .attr("height", h)
-      .append("g")
-      .attr("transform", `translate(${w/2},${h/2})`);
+      .append("g");
+    // .attr("transform", `translate(0,0)`);
+    // .attr("transform", `translate(${w/2},${h/2})`);
 
-    let radiusScale = d3.scaleSqrt().domain([Math.min(...z.filter(Number)), Math.max(...z.filter(Number))]).range([2, 6])
+    let radiusScale = d3
+      .scaleSqrt()
+      .domain([Math.min(...z.filter(Number)), Math.max(...z.filter(Number))])
+      .range([2, 5]);
 
     let circles = refAccess
       .selectAll("circle")
       .data(data)
       .enter()
       .append("circle")
+      .attr("opacity", 0.8)
       .attr("r", (d) => {
-        return radiusScale(parseFloat(d.fee)*10000)
+        return radiusScale(parseFloat(d.fee) * 8000);
       })
       .attr("fill", (d) => {
-        return d.network == 'ethereum' ? 'blue' : d.network == 'polygon' ? 'red' : 'white'
+        return d.tag == "collectible"
+          ? "blue"
+          : d.tag == "transaction"
+          ? "lightblue"
+          : d.tag == "exchange"
+          ? "gray"
+          : d.tag == "social"
+          ? "white"
+          : "yellow";
+      })
+      .call(
+        drag().on("start", dragstarted).on("drag", dragged).on("end", dragended)
+      );
+
+    function dragstarted(event, node) {
+      if (!event.active) simulation.alphaTarget(1).restart();
+      (node.fx = node.x), (node.fy = node.y);
+    }
+
+    function dragged(event, node) {
+      (node.fx = event.x), (node.fy = event.y);
+    }
+
+    function dragended(event, node) {
+      if (!event.active) simulation.alphaTarget(0);
+      (node.fx = null), (node.fy = null);
+    }
+
+    d3.select("#type_").on("click", () => {
+      circles.attr("fill", (d) => {
+        return d.type == "mint"
+          ? "green"
+          : d.type == "transfer"
+          ? "orange"
+          : d.type == "swap"
+          ? "gray"
+          : d.type == "vote"
+          ? "red"
+          : d.type == "poap"
+          ? "slategrey"
+          : d.type == "social"
+          ? "pink"
+          : d.type == "trade"
+          ? "lightblue"
+          : d.type == "post"
+          ? "purple"
+          : d.type == "vote"
+          ? "orangered"
+          : "white";
       });
+    });
+
+    d3.select("#tag_").on("click", () => {
+      circles.attr("fill", (d) => {
+        return d.tag == "collectible"
+          ? "blue"
+          : d.tag == "transaction"
+          ? "lightblue"
+          : d.tag == "exchange"
+          ? "gray"
+          : d.tag == "social"
+          ? "white"
+          : "yellow";
+      });
+    });
+
+    d3.select("#network_").on("click", () => {
+      circles.attr("fill", (d) => {
+        return d.network == "ethereum"
+          ? "blue"
+          : d.network == "polygon"
+          ? "brown"
+          : d.network == "xdai"
+          ? "gray"
+          : "white";
+      });
+    });
+
+    d3.select("#platform_").on("click", () => {
+      circles.attr("fill", (d) => {
+        return d.platform == "OpenSea"
+          ? "blue"
+          : d.platform == "MetaMask"
+          ? "lightblue"
+          : d.platform == "Uniswap"
+          ? "gray"
+          : d.platform == "SushiSwap"
+          ? "white"
+          : d.platform == "Lens"
+          ? "red"
+          : "orange";
+      });
+    });
 
     const ticked = () => {
-      circles.attr("cx", (d) => {
-        return d.x;
-      }).attr("cy", (d) => {
-        return d.y + 1;
-      });
+      circles
+        .attr("cx", (d) => {
+          return d.x;
+        })
+        .attr("cy", (d) => {
+          return d.y + 1;
+        });
     };
 
-    simulation.nodes(data)
-    .on('tick', ticked);
+    simulation.nodes(data).on("tick", ticked);
   }, []);
 
   return (
-
     <div
-      className={`w-[700px] h-[400px] flex flex-row justify-center items-center relative overflow-hidden`}
+      className={`min-w-[700px] h-[400px] flex flex-row justify-center items-center relative overflow-hidden`}
       onClick={() => {}}
     >
       <div
         className={`flex flex-row justify-center items-center`}
         ref={svgRef}
       ></div>
+
+      <div
+        className={`flex flex-row justify-center items-center w-full h-full bg-black/0 backdrop-blur-sm absolute bottom-0 opacity-30 pointer-events-none`}
+      />
+
+      <div
+        className={`w-[200px] h-[200px] absolute left-[30px] flex flex-col justify-center items-end rounded-[4px] pr-[30px] ${
+          dash_ ? "opacity-0 duration-400" : "opacity-80 duration-[1500ms]"
+        } transition-all`}
+        onClick={() => {}}
+      >
+        <div
+          className={`${
+            currentData == "tag" ? "w-[90px]" : "w-[20px] hover:w-[120px]"
+          } cursor-pointer duration-[200ms] transition-all my-[5px] flex flex-row relative overflow-hidden`}
+        >
+          <div
+            className={`w-[120px] h-full absolute left-0 pl-[30px] font-medium text-[13px] ${
+              isDark_ ? "text-white/40" : "text-black/60"
+            } flex flex-col justify-center hover:opacity-100 ${
+              currentData == "tag" ? "opacity-100" : "opacity-0"
+            } duration-300 transition-all`}
+            onClick={() => {
+              setCurrentData("tag");
+            }}
+            id={`tag_`}
+          >
+            Tag
+          </div>
+          <FontAwesomeIcon
+            icon={faTag}
+            className={`text-center duration-[200ms] transition-all ${
+              isDark_
+                ? "hover:text-white text-white/80"
+                : "hover:text-black text-black/80"
+            } w-[20px] h-[20px]`}
+          />
+        </div>
+
+        <div
+          className={`${
+            currentData == "network" ? "w-[90px]" : "w-[20px] hover:w-[120px]"
+          } cursor-pointer duration-[200ms] transition-all my-[5px] flex flex-row relative overflow-hidden`}
+        >
+          <div
+            className={`w-[120px] h-full absolute left-0 pl-[30px] font-medium text-[13px] ${
+              isDark_ ? "text-white/40" : "text-black/60"
+            } flex flex-col justify-center hover:opacity-100 ${
+              currentData == "network" ? "opacity-100" : "opacity-0"
+            } duration-300 transition-all`}
+            onClick={() => {
+              setCurrentData("network");
+            }}
+            id={`network_`}
+          >
+            Network
+          </div>
+          <FontAwesomeIcon
+            icon={faTowerBroadcast}
+            className={`text-center duration-[200ms] transition-all ${
+              isDark_
+                ? "hover:text-white text-white/80"
+                : "hover:text-black text-black/80"
+            } w-[20px] h-[20px]`}
+          />
+        </div>
+
+        <div
+          className={`${
+            currentData == "type" ? "w-[90px]" : "w-[20px] hover:w-[120px]"
+          } cursor-pointer duration-[200ms] transition-all my-[5px] flex flex-row relative overflow-hidden`}
+        >
+          <div
+            className={`w-[120px] h-full absolute left-0 pl-[30px] font-medium text-[13px] ${
+              isDark_ ? "text-white/40" : "text-black/60"
+            } flex flex-col justify-center hover:opacity-100 ${
+              currentData == "type" ? "opacity-100" : "opacity-0"
+            } duration-300 transition-all`}
+            onClick={() => {
+              setCurrentData("type");
+            }}
+            id={`type_`}
+          >
+            Type
+          </div>
+          <FontAwesomeIcon
+            icon={faBoxesStacked}
+            className={`text-center duration-[200ms] transition-all ${
+              isDark_
+                ? "hover:text-white text-white/80"
+                : "hover:text-black text-black/80"
+            } w-[20px] h-[20px]`}
+          />
+        </div>
+        <div
+          className={`${
+            currentData == "platform" ? "w-[90px]" : "w-[20px] hover:w-[120px]"
+          } cursor-pointer duration-[200ms] transition-all my-[5px] flex flex-col relative overflow-hidden`}
+        >
+          <div
+            className={`w-[120px] h-full absolute left-0 pl-[30px] font-medium text-[13px] ${
+              isDark_ ? "text-white/40" : "text-black/60"
+            } flex flex-col justify-center hover:opacity-100 ${
+              currentData == "platform" ? "opacity-100" : "opacity-0"
+            } duration-300 transition-all`}
+            onClick={() => {
+              setCurrentData("platform");
+              console.log(...getData("platform").keys());
+            }}
+            id={`platform_`}
+          >
+            Platform
+          </div>
+          <FontAwesomeIcon
+            icon={faCoins}
+            className={`text-center duration-[200ms] transition-all ${
+              isDark_
+                ? "hover:text-white text-white/80"
+                : "hover:text-black text-black/80"
+            } w-[20px] h-[20px]`}
+          />
+        </div>
+      </div>
+
+      <div
+        className={`flex flex-row justify-center items-center min-w-[300px] h-[80px] rounded-[4px] absolute bottom-[20px] ${dash_ ? "opacity-0 duration-400" : "opacity-80 duration-[1500ms]"} transition-all duration-400`}
+      >
+        <p
+          className={`min-w-[20px] h-full flex flex-col mx-[5px] justify-center items-center
+      ${isDark_ ? "text-white" : "text-black"} text-right relative bottom-[1px] text-[35px] font-black`}
+        >
+          {currentData.toUpperCase()}
+        </p>
+
+        <p
+          className={`text-[13px] w-[200px] ${
+            isDark_ ? "text-white/70" : "text-black"
+          } font-thin text-left`}
+        >
+          {currentData == "network"
+            ? "Transactions according to network used.."
+            : currentData == "platform"
+            ? "Transactions according to platform used.."
+            : currentData == "tag"
+            ? "Transactions according to category.."
+            : "Transactions according to nature of transaction.."}
+        </p>
+      </div>
+
+      <div className={`hidden w-[50px] h-[20px] bg-white/80 rounded-4px absolute`} id={'tooltip_'}>
+
+      </div>
       {/* <div className={`w-[100px] h-full flex flex-col justify-center items-end`} onClick={() => {}}>
         <div
           className={`${currentData == 'tag' ? 'w-[90px]' : 'w-[20px] hover:w-[120px]'} cursor-pointer duration-[200ms] transition-all my-[5px] flex flex-row relative overflow-hidden`}
--- components_new/user/components/Dashboard.tsx
+++ components_new/user/components/Dashboard.tsx
@@ -30,18 +30,19 @@ const Dashboard = ({ data }: DashboardProps) => {
   const ARWEAVE_URL = "/_next/image?url=https%3A%2F%2Farweave.net%2F";
   const [dash_, setDash_] = useRecoilState(extendDash);
 
-    // Everything on this element is Light/Dark theme ready..
-    const [isDark_, setIsDark_] = useRecoilState(isDarkMode);
+  // Everything on this element is Light/Dark theme ready..
+  const [isDark_, setIsDark_] = useRecoilState(isDarkMode);
 
   return (
     <div
       className={`w-[912px] h-[420px] flex flex-row justify-center items-center mx-auto`}
     >
-
       {/* Left Plate */}
 
       <div
-        className={`h-[412px] ${dash_ ? 'w-[452px]' : 'w-full'} rounded-[4px] ${isDark_ ? "bg-[#0f1729]" : "bg-[#e8e8e8]"} transition-all duration-200 overflow-hidden m-1 p-1 _container relative`}
+        className={`h-[412px] ${
+          dash_ ? "w-[452px]" : "w-full"
+        } rounded-[4px] transition-all duration-200 overflow-hidden m-0 p-0 _container relative`}
       >
         <img
           src={`${
@@ -54,30 +55,43 @@ const Dashboard = ({ data }: DashboardProps) => {
           } `}
         />
 
-        <div
-          className={`backdrop-blur-xl ${isDark_ ? "bg-[#0f1729]/30" : "bg-white/30"} h-full w-full absolute top-0 left-0`}
+        <d
+          className={`${isDark_ ? "invert" : "invert-0"} scale-[-1] h-[412px] ${
+            dash_ ? "w-[452px]" : "w-full"
+          } absolute left-0 top-0 _filter opacity-20 transition-all`}
         />
 
         <div
-          className={`w-full h-full absolute top-0 flex flex-row justify-center items-center transition-all duration-400 ${dash_ ? 'opacity-100 left-[0px]' : 'opacity-100 left-[0px]'}`}
+          className={`w-full h-full absolute top-0 flex flex-row justify-center items-center transition-all duration-400 ${
+            dash_ ? "opacity-100 left-[0px]" : "opacity-100 left-[0px]"
+          }`}
         >
-         {
-          data ?
-          <Visuals data={data.RSS3.transactions}/>
-          :
-          <div/>
-         }
+          {data ? <Visuals data={data.RSS3.transactions} /> : <div />}
         </div>
 
-         {/* Hidden Info Panel */}
         <div
-          className={`w-[452px] h-[70px] flex flex-col justify-center items-center absolute bottom-0 left-0 ${dash_ ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+          className={`${
+            isDark_ ? "bg-[#0f1729]/30" : "bg-white/30"
+          } ${currentANFT_ == -1 ? 'duration-[1000ms] backdrop-blur-none pointer-events-none' : 'duration-200 backdrop-blur-xl pointer-events-auto'} transition-all h-full w-full absolute top-0 left-0`}
+        />
+
+        {/* Hidden Info Panel */}
+        <div
+          className={`w-[452px] h-[70px] flex flex-col justify-center items-center absolute bottom-0 left-0 ${
+            dash_
+              ? "opacity-100 pointer-events-auto"
+              : "opacity-0 pointer-events-none"
+          }`}
         >
           {currentANFT_ != -1 ? (
             <a
               href={`https://koi.rocks/content-detail/${data?.ANFTS.koii[currentANFT_].id}`}
             >
-              <p className={`text-[14px] ${isDark_ ? 'text-white/30' : 'text-black/30'} font-thin`}>
+              <p
+                className={`text-[14px] ${
+                  isDark_ ? "text-white/30" : "text-black/30"
+                } font-thin`}
+              >
                 View on Koii
               </p>
             </a>
@@ -90,7 +104,13 @@ const Dashboard = ({ data }: DashboardProps) => {
             currentANFT_ != -1
               ? "top-0 duration-[800ms]"
               : "top-[-380px] duration-200"
-          } transition-all left-0 w-[452px] h-[350px] shadow-md flex flex-col ${isDark_ ? 'bg-[#0f1729]' : 'bg-[ghostwhite]'} justify-center items-center ${dash_ ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+          } transition-all left-0 w-[452px] h-[350px] shadow-md flex flex-col ${
+            isDark_ ? "bg-[#0f1729]" : "bg-[ghostwhite]"
+          } justify-center items-center ${
+            dash_
+              ? "opacity-100 pointer-events-auto"
+              : "opacity-0 pointer-events-none"
+          }`}
         >
           <div
             className={`w-full h-[60px] flex flex-row px-[20px] items-center`}
@@ -115,12 +135,18 @@ const Dashboard = ({ data }: DashboardProps) => {
               {currentANFT_ != -1 ? (
                 <div className={``}>
                   <p
-                    className={`font-semibold ${isDark_ ? 'text-white/80' : 'text-black/80'} text-[15px] w-[250px] truncate`}
+                    className={`font-semibold ${
+                      isDark_ ? "text-white/80" : "text-black/80"
+                    } text-[15px] w-[250px] truncate`}
                   >
                     {data?.ANFTS.koii[currentANFT_].title}
                   </p>
                   <div className={`h-[13px] flex flex-row`}>
-                    <p className={`font-thin ${isDark_ ? 'text-white/40' : 'text-black/40'} text-[12px]`}>
+                    <p
+                      className={`font-thin ${
+                        isDark_ ? "text-white/40" : "text-black/40"
+                      } text-[12px]`}
+                    >
                       {`Acquired on ${new Converter.date(
                         data?.ANFTS.koii[currentANFT_].timestamp
                       ).getDay()} ${
@@ -137,7 +163,9 @@ const Dashboard = ({ data }: DashboardProps) => {
                       ).getYear()}`}
                     </p>
                     <p
-                      className={`ml-1 font-medium italic ${isDark_ ? 'text-white/80' : 'text-black/80'} text-[12px]`}
+                      className={`ml-1 font-medium italic ${
+                        isDark_ ? "text-white/80" : "text-black/80"
+                      } text-[12px]`}
                     >
                       {"(" +
                         data?.ANFTS.koii[currentANFT_].poster.slice(0, 4) +
@@ -175,7 +203,9 @@ const Dashboard = ({ data }: DashboardProps) => {
               <div />
             )}
             <div
-              className={`backdrop-blur-xl ${isDark_ ? 'bg-[#0f1729]/30' : 'bg-white/30'} w-full h-full absolute top-0 left-0
+              className={`backdrop-blur-xl ${
+                isDark_ ? "bg-[#0f1729]/30" : "bg-white/30"
+              } w-full h-full absolute top-0 left-0
             ${
               viewDesc_
                 ? "duration-[200ms] opacity-100"
@@ -191,7 +221,9 @@ const Dashboard = ({ data }: DashboardProps) => {
             >
               {currentANFT_ != -1 ? (
                 <div
-                  className={`w-[320px] h-full text-center mx-auto font-black ${isDark_ ? 'text-white/60' : 'text-black/60'} text-[15px] flex flex-col justify-center items-center`}
+                  className={`w-[320px] h-full text-center mx-auto font-black ${
+                    isDark_ ? "text-white/60" : "text-black/60"
+                  } text-[15px] flex flex-col justify-center items-center`}
                 >
                   {data?.ANFTS.koii[currentANFT_].description}
                 </div>
@@ -203,38 +235,49 @@ const Dashboard = ({ data }: DashboardProps) => {
           <div className={`w-full h-[38px]`} />
         </div>
       </div>
-      
+
       {/* Right Plate */}
 
       <div
-        className={`h-[412px] ${dash_ ? 'w-[457px] m-1 p-1' : 'w-0 m-0 p-0'} rounded-[4px] ${isDark_ ? "bg-[#0f1729]" : "bg-[#e8e8e8]"} transition-all duration-200 _container _grid`}
+        className={`h-[412px] ${
+          dash_ ? "w-[457px] m-1 p-1" : "w-0 m-0 p-0"
+        } transition-all rounded-[4px] relative`}
       >
-        {data?.ANFTS.koii.map((nft, i) => {
-          return (
-            <div
-              key={nft.id}
-              className={`w-[145px] h-[145px] overflow-hidden relative rounded-[3px] cursor-pointer flex flex-row justify-center items-center transition-all ${
-                currentANFT_ == i
-                  ? "opacity-100 duration-[500ms]"
-                  : "opacity-30 duration-[200ms]"
-              }`}
-              onClick={() => {
-                if (currentANFT_ == i) {
-                  setCurrentANFT_(-1);
-                  setViewDesc_(false);
-                } else {
-                  setCurrentANFT_(i);
-                  setViewDesc_(false);
-                }
-              }}
-            >
-              <img
-                className={`w-full h-full object-cover`}
-                src={ARWEAVE_URL + nft.id + "&w=3840&q=75"}
-              />
-            </div>
-          );
-        })}
+        <div
+          className={`${
+            isDark_ ? "invert" : "invert-0"
+          } h-full w-full scale-y-[-1] _filter opacity-20 transition-all rounded-[4px] absolute top-0 right-0`}
+        ></div>
+        <div
+          className={`w-full h-full rounded-[4px] transition-all duration-200 _container _grid absolute top-0 right-0 m-0 pt-[1px] pl-[1px]`}
+        >
+          {data?.ANFTS.koii.map((nft, i) => {
+            return (
+              <div
+                key={nft.id}
+                className={`w-[145px] h-[145px] overflow-hidden relative rounded-[3px] cursor-pointer flex flex-row justify-center items-center transition-all ${
+                  currentANFT_ == i
+                    ? "opacity-100 duration-[500ms]"
+                    : "opacity-80 duration-[200ms]"
+                }`}
+                onClick={() => {
+                  if (currentANFT_ == i) {
+                    setCurrentANFT_(-1);
+                    setViewDesc_(false);
+                  } else {
+                    setCurrentANFT_(i);
+                    setViewDesc_(false);
+                  }
+                }}
+              >
+                <img
+                  className={`w-full h-full object-cover`}
+                  src={ARWEAVE_URL + nft.id + "&w=3840&q=75"}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
